33cdfabd0af708c26d53081f4b1d1dc7bf0b5a55
Josh Perakis <32339145+joshuaPerakis@users.noreply.github.com>
2022-10-10 17:36:45 -0500

    Update apigw.tf

--- apigw.tf
+++ apigw.tf
@@ -1,20 +1,9 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-resource "aws_elasticache_cluster" "cluster-redis" {
-  cluster_id           = "cluster-redis"
-  engine               = "redis"
-  node_type            = "cache.m4.large"
-  num_cache_nodes      = 1
-  engine_version       = "3.2.10"
-  at_rest_encryption_enabled = true
+resource "aws_apigatewayv2_api" "example" {
+  name                       = "example-websocket-api"
+  protocol_type              = "WEBSOCKET"
 }
 
-resource "aws_elasticache_cluster" "cluster-memcache" {
-  cluster_id           = "cluster-memcache"
-  engine               = "memcached"
-  node_type            = "cache.m4.large"
-  num_cache_nodes      = 1
-  transit_encryption_enabled = true
+resource "aws_apigatewayv2_api" "example" {
+  name          = "example-http-api"
+  protocol_type = "HTTP"
 }

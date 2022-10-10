89791bc08e39797ea04927334b72fb9950d36928
Josh Perakis <32339145+joshuaPerakis@users.noreply.github.com>
2022-10-10 14:33:18 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,6 +6,6 @@ resource "aws_s3_bucket" "terraform-testing123-321" {
    bucket = "terraform-testing123-321"
 }
 
-output "bucket-name" {
-  value =  resource.aws_s3_bucket.terraform-testing123-321
-}
+# output "bucket-name" {
+#   value =  resource.aws_s3_bucket.terraform-testing123-321
+# }

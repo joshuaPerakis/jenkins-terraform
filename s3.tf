aaace3d755763e7fd0f4775af3edbe2a76587f21
Josh Perakis <32339145+joshuaPerakis@users.noreply.github.com>
2022-10-10 13:36:35 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "teraform_testing123" {
-   bucket = "teraform_testing123"
+resource "aws_s3_bucket" "teraform_testing123_321" {
+   bucket = "teraform_testing123_321"
 }

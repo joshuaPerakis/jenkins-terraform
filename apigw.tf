d6485ae11862a7c08e3e56ff8d20fbc1c12b264f
Josh Perakis <32339145+joshuaPerakis@users.noreply.github.com>
2022-10-10 17:06:35 -0500

    Update apigw.tf

--- apigw.tf
+++ apigw.tf
@@ -1,6 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
 resource "aws_elasticache_cluster" "cluster-redis" {
   cluster_id           = "cluster-redis"

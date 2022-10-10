7824d41f6e68770a6c209e60d9f659cbfd2c8a1e
Josh Perakis <32339145+joshuaPerakis@users.noreply.github.com>
2022-10-10 17:54:17 -0500

    Update apigw.tf

--- apigw.tf
+++ apigw.tf
@@ -1,9 +1,9 @@
-resource "aws_apigatewayv2_api" "example" {
-  name                       = "example-websocket-api"
-  protocol_type              = "WEBSOCKET"
-}
-
 # resource "aws_apigatewayv2_api" "example" {
-#   name          = "example-http-api"
-#   protocol_type = "HTTP"
+#   name                       = "example-websocket-api"
+#   protocol_type              = "WEBSOCKET"
 # }
+
+resource "aws_apigatewayv2_api" "example" {
+  name          = "example-http-api"
+  protocol_type = "HTTP"
+}

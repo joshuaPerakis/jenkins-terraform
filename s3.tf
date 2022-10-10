20895e5e01a9747ce207a8d3facff19c0e130df0
Josh Perakis <32339145+joshuaPerakis@users.noreply.github.com>
2022-10-10 13:47:29 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -5,3 +5,7 @@ provider "aws" {
 resource "aws_s3_bucket" "terraform-testing123-321" {
    bucket = "terraform-testing123-321"
 }
+
+output "bucket-name" {
+  value =  terraform-testing123-321.bucket
+}

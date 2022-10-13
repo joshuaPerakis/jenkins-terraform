9e9a23fd5e631c1092dda1f77bed1078039660a8
Josh Perakis <32339145+joshuaPerakis@users.noreply.github.com>
2022-10-13 16:38:47 -0500

    Delete apigw.tf

--- terraform/apigw.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_apigatewayv2_api" "example" {
-#   name                       = "example-websocket-api"
-#   protocol_type              = "WEBSOCKET"
-# }
-
-resource "aws_apigatewayv2_api" "example" {
-  name          = "example-http-api"
-  protocol_type = "HTTP"
-}

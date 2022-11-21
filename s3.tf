e9e0eda844d6c31834ae68dddd341ad7f391eacf
joshuaperakis <jvperakis@comcast.net>
2022-11-21 13:58:41 -0600

    exception

--- s3.tf
+++ s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "terraform-testing123-321" {
+   #checkov:skip=CKV_AWS_19:Exception for this bucket
    bucket = "terraform-testing123-321"
    tags   = {
       test = "test"

1d5f221dfc5e14c2b4c8aee74222b659f4246a75
joshuaperakis <jvperakis@comcast.net>
2022-11-29 00:03:22 -0600

    exception

--- terraform_resources/kms.tf
+++ terraform_resources/kms.tf
@@ -5,6 +5,13 @@ resource "aws_kms_key" "a" {
 }
 
 resource "aws_kms_key" "b" {
+    description             = "non rotated key with exception"
+    deletion_window_in_days = 7
+    enable_key_rotation     = false
+    #checkov:skip=CKV_AWS_7:Test skipping kms rotation for this specific key
+}
+
+resource "aws_kms_key" "c" {
     description             = "rotated key"
     deletion_window_in_days = 7
     enable_key_rotation     = true
